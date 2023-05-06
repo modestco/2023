3c9094eb13938d9cb2efba8ba29664612960c021
Modestly Jen <jen@jenniferluchka.com>
2023-05-06 12:11:35 -0400

    fixing errors

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -7,29 +7,26 @@ export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
-      <ul>
-        <li>
-          <FormattedDate date={props.data.date} />
-        </li>
-        <li className="text-capitalize">{props.data.description}</li>
-      </ul>
-      <div className="row mt-3">
+      <div>
+        <ul className="description">
+          <li>
+            Current time: <FormattedDate date={props.data.date} />,{" "}
+            {props.data.description}
+          </li>
+          <li>
+            Humidity: <strong>{props.data.humidity}%</strong>, Wind:{" "}
+            <strong>{props.data.wind}km/h</strong>
+          </li>
+        </ul>
+      </div>
+      <div className="row">
         <div className="col-6">
-          <div className="clearfix">
+          <span>
             <div className="float-left">
               <WeatherIcon code={props.data.icon} size={52} />
-            </div>
-
-            <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
-          </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind} km/h</li>
-          </ul>
+          </span>
         </div>
       </div>
     </div>
